fe88f0ceeac8612380324b7f52f735aebde13d5d
Gregor von Laszewski <laszewski@gmail.com>
2020-11-25 15:26:36 -0500

    docker

--- ubuntu-20.04/031-docker.sh
+++ ubuntu-20.04/031-docker.sh
@@ -2,10 +2,14 @@
 
 banner docker
 
-sudo apt update
-sudo apt install docker.io
+sudo apt update 
+sudo apt install docker.io -y
+sudo groupadd docker
+
 sudo systemctl enable --now docker
 # sudo systemctl disable --now docker  # disable it
+newgrp docker
+
 sudo usermod -aG docker $USER
 docker --version
 docker run hello-world
